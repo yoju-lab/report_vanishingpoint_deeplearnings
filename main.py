c577faeb6057edfd120c2523b5e6efd1395654f9
Sanghun Oh <otter.oh@gmail.com>
2022-11-06 20:40:22 +0900

    add search keys

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     search_keys = list(set(search_list))
 
     #Parameters
-    number_of_images = 70                # Desired number of images
+    number_of_images = 50                # Desired number of images
     headless = False                     # True = No Chrome GUI
     min_resolution = (0, 0)             # Minimum desired image resolution
     max_resolution = (9999, 9999)       # Maximum desired image resolution
